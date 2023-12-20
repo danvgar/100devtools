904b867000bd717a1b058bece10fb5bbc5635240
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2023-12-19 18:48:25 -0800

    npm init and installed react, react-dom dependencies

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,34 @@
-const Tool = new
+// **********
+// Tool Constructor
+// **********
+
+// Properties
+// ----------
+
+// Methods
+// ----------
+
+
+
+// ===================================
+
+// **********
+// User Constructor?
+// **********
+// Authentication?
+
+// Properties
+// ----------
+// Number of tag-votes per tool
+
+// Methods
+// ----------
+// Check if user has already voted for three tags per object
+// Check if user has already voted up/down a tool
+
+
+
+// Components
+//  - Tags
+//  - Tools Object
+//  - 
