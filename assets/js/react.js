702045a8271929109fb1674b5728b1cab9c0f5dc
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2023-12-22 17:41:21 -0800

    Significant updates to AppTags and AppLinks components

--- assets/js/react.js
+++ assets/js/react.js
@@ -1,18 +1,56 @@
 // import React, { Component } from "react";
 // import { render } from "react-dom";
 
-function Button() {
-    return <button>Tag 1</button>
+let tags = ["stream", "search", "AI"]
+
+let links = [
+    {
+        id: 1,
+        title: "Google",
+        url: "https://www.google.com/",
+        description: "description",
+    },
+    {
+        id: 2,
+        title: "ChatGPT",
+        url: "https://chat.openai.com/",
+        description: "description",
+    },
+    {
+        id: 3,
+        title: "YouTube",
+        url: "https://www.youtube.com/",
+        description: "description",
+    },
+
+]
+
+function Tag({ tagName }) {
+    return <button>{tagName}</button>
+}
+
+function Link({ linkIndex }) {
+    return <p>{links[linkIndex].title}</p>
+}
+
+function AppTags() {
+    return <>
+        {tags.map(item => (
+            <button key={item}>{item.toLowerCase()}</button>
+        ))}
+    </>
 }
 
 function AppLinks() {
-    return (<>
-        <p>Link 1</p>
-        <p>Link 2</p>
-        <p>Link 3</p>
-    </>)
+    return <>
+        <ul>
+            {links.map(obj => (
+                <li key={obj.id}>{obj.title}</li>
+            ))}
+        </ul>
+    </>
 }
 
-ReactDOM.render(<Button />, document.getElementById("app-tags"))
+ReactDOM.render(<AppTags />, document.getElementById("app-tags"))
 
 ReactDOM.render(<AppLinks />, document.getElementById("app-links"))
