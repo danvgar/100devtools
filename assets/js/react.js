8fb6904bf88a8c9d7aaa92c533f5e52f6861bd2c
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2023-12-22 17:25:09 -0800

    Testing components. modified HTML and CSS a bit to app-links and app-tags

--- assets/js/react.js
+++ assets/js/react.js
@@ -2,8 +2,17 @@
 // import { render } from "react-dom";
 
 function Button() {
-    return <button>Test</button>
-
+    return <button>Tag 1</button>
 }
 
-ReactDOM.render(<Button />, app)
+function AppLinks() {
+    return (<>
+        <p>Link 1</p>
+        <p>Link 2</p>
+        <p>Link 3</p>
+    </>)
+}
+
+ReactDOM.render(<Button />, document.getElementById("app-tags"))
+
+ReactDOM.render(<AppLinks />, document.getElementById("app-links"))
